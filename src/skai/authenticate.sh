828896c52eac840a3cd3e6d0ceccf44c314e28ad
Skai team <no-reply@google.com>
2022-06-09 16:29:33 +0000

    Internal change
    PiperOrigin-RevId: 453943765

--- src/skai/authenticate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Simple script which handles setting authentication variables for GDAL.
-
-echo "Authorization: Bearer $(gcloud auth application-default print-access-token)" >/tmp/cloud_header
-export GDAL_HTTP_HEADER_FILE=/tmp/cloud_header
