3188f07dee76e7f36e1eeeb1c662513030fa0daa
ambaha1 <baha.amine@hotmail.fr>
2023-01-20 09:27:41 +0100

    Update src/colab/colab_utils.py
    Co-authored-by: Joseph Xu <jzxu@google.com>

--- src/colab/colab_utils.py
+++ src/colab/colab_utils.py
@@ -59,7 +59,7 @@ def launch_pexpect_process(script,
       for argument in arguments
   ]
   commands = '; '.join([
-      f'set -e', f'source {dir_args["python_env"]}',
+      'set -e', f'source {dir_args["python_env"]}',
       f'export GOOGLE_APPLICATION_CREDENTIALS={dir_args["path_cred"]}',
       f'python {dir_args["path_skai"]}/src/{script[0]} {flags_str[0]}'
   ])
