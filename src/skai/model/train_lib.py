a945f49fb8f027f136f9edf4507c439c6687f5ed
panford <panfordkobby@gmail.com>
2023-11-03 12:22:08 +0000

    fix linting

--- src/skai/model/train_lib.py
+++ src/skai/model/train_lib.py
@@ -973,3 +973,4 @@ def train_and_evaluate(
     evaluate_model(two_head_model, output_dir, dataloader.eval_ds,
                    save_model_checkpoints, save_best_model)
     return two_head_model
+  
