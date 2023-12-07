17eb3543d1b9953b264b85d2e33ee28303bd3d7a
panford <panfordkobby@gmail.com>
2023-12-07 09:32:25 +0000

    migrate example ids from byte to ints

--- src/skai/model/inference_lib.py
+++ src/skai/model/inference_lib.py
@@ -372,7 +372,7 @@ def _example_to_row(
   Returns:
     Inference row.
   """
-  example_id = utils.get_bytes_feature(example, 'example_id')[0].decode()
+  example_id = utils.get_int64_feature(example, 'example_id')[0]
   building_id = utils.get_bytes_feature(example, 'encoded_coordinates')[
       0
   ].decode()
