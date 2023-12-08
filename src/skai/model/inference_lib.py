8fb81cdfcb58dddf9a3cca2d9c93e10b368c3b84
panford <panfordkobby@gmail.com>
2023-12-08 07:20:33 +0000

    fix bug on example_id

--- src/skai/model/inference_lib.py
+++ src/skai/model/inference_lib.py
@@ -372,7 +372,7 @@ def _example_to_row(
   Returns:
     Inference row.
   """
-  example_id = utils.get_int64_feature(example, 'example_id')
+  example_id = utils.get_int64_feature(example, 'int64_id')[0]
   building_id = utils.get_bytes_feature(example, 'encoded_coordinates')[
       0
   ].decode()
